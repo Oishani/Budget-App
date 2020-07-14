263caca06818795898e0bdaca94a92ead1a03f34
Oishani <oishani.ganguly@gmail.com>
2020-07-14 08:58:02 +0530

    Creating an Init Function

--- app.js
+++ app.js
@@ -2,15 +2,56 @@
 var budgetController = (function () {})();
 
 // UI CONTROLLER
-var UIController = (function () {})();
+var UIController = (function () {
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn',
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value),
+      };
+    },
+  };
+
+  //   getDOMstrings: function() {
+  //     return DOMstrings;
+  // }
+})();
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+  var setupEventListeners = function () {
+    var DOM = UICtrl.getDOMstrings();
 
-  document.addEventListener('keypress', function (event) {
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
-    }
-  });
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  var ctrlAddItem = function () {
+    var input;
+
+    // 1. Get the field input data
+    input = UICtrl.getInput();
+  };
+
+  return {
+    init: function () {
+      console.log('Application has started.');
+      setupEventListeners();
+    },
+  };
 })(budgetController, UIController);
+
+controller.init();
