91350608c2421bd5dfee9b235b32600dd0885dac
Oishani <oishani.ganguly@gmail.com>
2020-07-14 09:15:15 +0530

    Adding a New Item to Budget Controller

--- app.js
+++ app.js
@@ -22,6 +22,36 @@ var budgetController = (function () {
       inc: 0,
     },
   };
+
+  return {
+    addItem: function (type, des, val) {
+      var newItem, ID;
+
+      //[1 2 3 4 5], next ID = 6
+      //[1 2 4 6 8], next ID = 9
+      // ID = last ID + 1
+
+      // Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Create new item based on 'inc' or 'exp' type
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+
+      // Push it into our data structure
+      data.allItems[type].push(newItem);
+
+      // Return the new element
+      return newItem;
+    },
+  };
 })();
 
 // UI CONTROLLER
@@ -63,10 +93,13 @@ var controller = (function (budgetCtrl, UICtrl) {
   };
 
   var ctrlAddItem = function () {
-    var input;
+    var input, newItem;
 
     // 1. Get the field input data
     input = UICtrl.getInput();
+
+    // 2. Add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
   };
 
   return {
