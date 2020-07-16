f92905a2868a5661a69bd56f59a2c37f9743da7c
Oishani <oishani.ganguly@gmail.com>
2020-07-16 08:02:49 +0530

    Formatting Budget Numbers

--- app.js
+++ app.js
@@ -164,6 +164,38 @@ var UIController = (function () {
     dateLabel: '.budget__title--month',
   };
 
+  var formatNumber = function (num, type) {
+    var numSplit, int, dec, type;
+    /*
+        + or - before number
+        exactly 2 decimal points
+        comma separating the thousands
+
+        2310.4567 -> + 2,310.46
+        2000 -> + 2,000.00
+        */
+
+    num = Math.abs(num);
+    num = num.toFixed(2);
+
+    numSplit = num.split('.');
+
+    int = numSplit[0];
+    if (int.length > 3) {
+      int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3); //input 23510, output 23,510
+    }
+
+    dec = numSplit[1];
+
+    return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
+  };
+
+  var nodeListForEach = function (list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
+    }
+  };
+
   return {
     getInput: function () {
       return {
