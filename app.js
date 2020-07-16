76e21f3abfec16031620bc2f71258e183cfa6c6d
Oishani <oishani.ganguly@gmail.com>
2020-07-16 07:28:47 +0530

    Deleting an Item from Budget Controller

--- app.js
+++ app.js
@@ -62,6 +62,25 @@ var budgetController = (function () {
       return newItem;
     },
 
+    deleteItem: function (type, id) {
+      var ids, index;
+
+      // id = 6
+      //data.allItems[type][id];
+      // ids = [1 2 4  8]
+      //index = 3
+
+      ids = data.allItems[type].map(function (current) {
+        return current.id;
+      });
+
+      index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+    },
+
     calculateBudget: function () {
       // Calculate total income and expenses
       calculateTotal('exp');
