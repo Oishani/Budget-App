7bd35521d8745856de0931f2e832c4f47ad44684
Oishani <oishani.ganguly@gmail.com>
2020-07-16 07:51:49 +0530

    Updating the Percentages: UI Controller

--- app.js
+++ app.js
@@ -243,6 +243,18 @@ var UIController = (function () {
       }
     },
 
+    displayPercentages: function (percentages) {
+      var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+      nodeListForEach(fields, function (current, index) {
+        if (percentages[index] > 0) {
+          current.textContent = percentages[index] + '%';
+        } else {
+          current.textContent = '---';
+        }
+      });
+    },
+
     getDOMstrings: function () {
       return DOMstrings;
     },
